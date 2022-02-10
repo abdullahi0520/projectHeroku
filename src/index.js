c0c25d014532ee2c68a1027e54a6cabaf15e9ff0
abdullahi0520 <90243393+abdullahi0520@users.noreply.github.com>
2022-02-10 12:22:18 -0600

    connected

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+const express = require('express');
+const server = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT
+
+server.listen(PORT, ()=> {
+    console.log(`listening on ${PORT}`);
+    console.log(process.env.MESSAGE)
+    
+})
+server.use('*', (req,res)=> {
+    res.send('<h1>Hello</h1>');
+})
